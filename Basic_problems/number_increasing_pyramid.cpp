ba2dfcd6e1ad4ed750dd237088ad73119aef17fc
manshukumar893-commits <manshukumar893@gmail.com>
2026-04-08 14:50:49 +0530

    Create number_increasing_pyramid.cpp

---
+++ Basic_problems/number_increasing_pyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the positive value of a:"<<endl;
+    cin>>a;
+    for(int i = 1;i<=a;i++){
+        for(int j = a;j>i;j--){
+            cout<<" ";
+        }
+        for()
+        
+        
+        cout<<"\n";
+    }
+}
